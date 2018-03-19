d5792b69e0f7ecb1e575b44ac0270e09a5a9ebf8
byio <ben@benyang.io>
2018-03-19 09:56:07 +0800

    set up client side to emit chat data to sockets

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,16 @@
 // Make connection
 const socket = io.connect('http://localhost:4000');
+
+// Query DOM
+const message = document.getElementById('message');
+const handle = document.getElementById('handle');
+const btn = document.getElementById('send');
+const output = document.getElementById('output');
+
+// Emit Events
+btn.addEventListener('click', () => {
+  socket.emit('chat', {
+    message: message.value,
+    handle: handle.value
+  });
+});
