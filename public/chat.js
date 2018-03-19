b4e786c18c4cd540cfdd79a46fdd8de23486d15e
byio <ben@benyang.io>
2018-03-19 10:33:50 +0800

    listen out for user typing a message on client, and emit a socket event from client to server

--- public/chat.js
+++ public/chat.js
@@ -6,11 +6,18 @@ const message = document.getElementById('message');
 const handle = document.getElementById('handle');
 const btn = document.getElementById('send');
 const output = document.getElementById('output');
+const feedback = document.getElementById('feedback');
 
 // Emit Events
 btn.addEventListener('click', () => {
   socket.emit('chat', {
     message: message.value,
+    handle: handle.value
+  });
+});
+
+message.addEventListener('keypress', () => {
+  socket.emit('typing', {
     handle: handle.value
   });
 });
